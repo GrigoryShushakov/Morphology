d93a9c1c14f533dee6016885ce8916e3799e19c2
Warren Moore <wm@warrenmoore.net>
2013-02-21 10:03:29 -0500

    Initial Commit

---
+++ Morphology/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Morphology
+//
+//  Created by Warren Moore on 2/21/13.
+//  Copyright (c) 2013 Auerhaus Development, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
